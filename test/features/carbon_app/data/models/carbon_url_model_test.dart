ab93c6f889e32493e5f00ab821fa586d50a7a098
LuisHenrique01 <luizbueno1201@gmail.com>
2022-06-23 00:56:37 -0300

    Tests passed

--- test/features/carbon_app/data/models/carbon_url_model_test.dart
+++ test/features/carbon_app/data/models/carbon_url_model_test.dart
@@ -1,12 +1,36 @@
+import 'dart:convert';
 import 'package:carbon_app/features/carbon_app/data/models/carbon_url_model.dart';
 import 'package:carbon_app/features/carbon_app/domain/entities/carbon_url.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../fixtures/fixtures_reader.dart';
+
 void main() {
   final tCarbonUrlModel = CarbonUrlModel(
-      url: 'www.google.com.br', green: true, bytes: 1500, cleanerThan: 0.85);
+      url: "https://www.google.com.br/",
+      green: true,
+      bytes: 807022,
+      cleanerThan: 0.83);
 
   test('Test model carbonUrl', () async {
     expect(tCarbonUrlModel, isA<CarbonUrl>());
+  });
+
+  test('fromJson', () async {
+    final Map<String, dynamic> jsonMap =
+        json.decode(fixture('carbon_url.json'));
+    final result = CarbonUrlModel.fromJson(jsonMap);
+    expect(result, tCarbonUrlModel);
+  });
+
+  test('toJson', () async {
+    final result = tCarbonUrlModel.toJson();
+    final expectedJsonMap = {
+      "url": "https://www.google.com.br/",
+      "green": true,
+      "bytes": 807022,
+      "cleanerThan": 0.83
+    };
+    expect(result, expectedJsonMap);
   });
 }
--- test/features/carbon_app/data/models/data_statistics_model_test.dart
+++ test/features/carbon_app/data/models/data_statistics_model_test.dart
@@ -1,17 +1,52 @@
+import 'dart:convert';
+
 import 'package:carbon_app/features/carbon_app/data/models/data_statistics.dart';
 import 'package:carbon_app/features/carbon_app/domain/entities/data_statistics.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../fixtures/fixtures_reader.dart';
+
 void main() {
   final tGridModel =
       GridModel(grams: 0.0002517412416636944, litres: 0.00014001847861334682);
-  final tCo2Model = Co2Model(grid: tGridModel, renewable: tGridModel);
-  final tStatistics = Statistics(
+  final tGridRenewableModel =
+      GridModel(grams: 0.000218251682817936, litres: 0.00012139158598333597);
+  final tCo2Model = Co2Model(grid: tGridModel, renewable: tGridRenewableModel);
+  final tStatistics = StatisticsModel(
       adjustedBytes: 755, energy: 5.69550320506096e-7, co2: tCo2Model);
   final tDataStatistics =
       DataStatisticsModel(statistics: tStatistics, cleanerThan: 1);
 
   test('Test data statistics model', () async {
     expect(tDataStatistics, isA<DataStatistics>());
+  });
+
+  test('fromJson', () async {
+    final Map<String, dynamic> jsonMap =
+        json.decode(fixture('data_statistics.json'));
+    final result = DataStatisticsModel.fromJson(jsonMap);
+    expect(result, tDataStatistics);
+  });
+
+  test('toJson', () async {
+    final result = tDataStatistics.toJson();
+    final expectedJsonMap = {
+      "statistics": {
+        "adjustedBytes": 755,
+        "energy": 5.69550320506096e-7,
+        "co2": {
+          "grid": {
+            "grams": 0.0002517412416636944,
+            "litres": 0.00014001847861334682
+          },
+          "renewable": {
+            "grams": 0.000218251682817936,
+            "litres": 0.00012139158598333597
+          }
+        }
+      },
+      "cleanerThan": 1
+    };
+    expect(result, expectedJsonMap);
   });
 }
