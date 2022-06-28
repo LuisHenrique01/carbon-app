2eae553b2d92aaa456827d1ac0d215d14e5b32b9
LuisHenrique01 <luizbueno1201@gmail.com>
2022-06-27 22:13:15 -0300

    coder 5 e 6

--- test/features/carbon_app/domain/usecases/get_concrete_carbon_url_impl_test.dart
+++ test/features/carbon_app/domain/usecases/get_concrete_carbon_url_impl_test.dart
@@ -1,3 +1,6 @@
+import 'package:carbon_app/core/platform/network_info.dart';
+import 'package:carbon_app/features/carbon_app/data/datasources/carbon_url_local_data_sources.dart';
+import 'package:carbon_app/features/carbon_app/data/datasources/carbon_url_remote_data_sources.dart';
 import 'package:carbon_app/features/carbon_app/data/repositories/carbon_url_repository.dart';
 import 'package:carbon_app/features/carbon_app/domain/repositories/carbon_url_repository.dart';
 import 'package:carbon_app/features/carbon_app/domain/usecases/get_concrete_carbon_url.dart';
@@ -7,9 +10,19 @@ import 'package:mockito/mockito.dart';
 
 class MockCarbonUrlRepository extends Mock implements CarbonUrlRepository {}
 
+class MockRemoteDataSource extends Mock implements CarbonUrlRemoteDataSource {}
+
+class MockLocalDataSource extends Mock implements CarbonUrlLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
 @GenerateMocks([MockCarbonUrlRepository])
 void main() {
-  final repository = CarbonUrlRepositoryImpl();
+  final repository = CarbonUrlRepositoryImpl(
+    remoteDataSource: MockRemoteDataSource(),
+    localDataSource: MockLocalDataSource(),
+    networkInfo: MockNetworkInfo(),
+  );
   final usecase = GetConcreteCarbonUrl(repository);
 
   const String url = 'https://www.google.com.br/';
--- test/features/carbon_app/domain/usecases/get_concrete_data_statistics_impl_test.dart
+++ test/features/carbon_app/domain/usecases/get_concrete_data_statistics_impl_test.dart
@@ -1,3 +1,6 @@
+import 'package:carbon_app/core/platform/network_info.dart';
+import 'package:carbon_app/features/carbon_app/data/datasources/data_statistics_local_data_sources.dart';
+import 'package:carbon_app/features/carbon_app/data/datasources/data_statistics_remote_data_sources.dart';
 import 'package:carbon_app/features/carbon_app/data/repositories/data_statistics_repository.dart';
 import 'package:carbon_app/features/carbon_app/domain/repositories/data_statistcs_repository.dart';
 import 'package:carbon_app/features/carbon_app/domain/usecases/get_concrete_data_statistics.dart';
@@ -8,9 +11,21 @@ import 'package:mockito/mockito.dart';
 class MockDataStatisticsRepository extends Mock
     implements DataStatisticsRepository {}
 
+class MockRemoteDataSource extends Mock
+    implements DataStatisticsRemoteDataSource {}
+
+class MockLocalDataSource extends Mock
+    implements DataStatisticsLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
 @GenerateMocks([MockDataStatisticsRepository])
 void main() {
-  final repository = DataStatisticsRepositoryImpl();
+  final repository = DataStatisticsRepositoryImpl(
+    remoteDataSource: MockRemoteDataSource(),
+    localDataSource: MockLocalDataSource(),
+    networkInfo: MockNetworkInfo(),
+  );
   final usecase = GetConcreteDataStatistics(repository);
 
   const int bytes = 1000;
